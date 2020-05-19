213110ada9633629fb46ffcc4c3bd35af52361af
SmartSunCong <yangcong_hit@163.com>
2020-05-19 09:46:43 +0800

    sc deom

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/SmartsYoung/service-center-demo
+
+require (
+	github.com/apache/servicecomb-service-center v0.0.0-20190923140639-79cd6fe81a2d
+	github.com/go-chassis/paas-lager v0.0.0-20181123014243-005283cca84c // indirect
+	github.com/go-mesh/openlogging v0.0.0-20181122085847-3daf3ad8ed35 // indirect
+	github.com/go-yaml/yaml v2.1.0+incompatible
+	github.com/gorilla/websocket v1.4.0 // indirect
+	github.com/prometheus/procfs v0.0.0-20190104112138-b1a0a9a36d74 // indirect
+	golang.org/x/net v0.0.0-20190110200230-915654e7eabc // indirect
+	golang.org/x/sys v0.0.0-20181116152217-5ac8a444bdc5 // indirect
+	google.golang.org/genproto v0.0.0-20181221175505-bd9b4fb69e2f // indirect
+	google.golang.org/grpc v1.17.0 // indirect
+)
+
+go 1.13
---
+++ helloworld/rest/provider/provider.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/SmartsYoung/service-center-demo/helloworld/rest/config"
+	"github.com/SmartsYoung/service-center-demo/helloworld/rest/servicecenter"
+)
+
+var conf *config.Config
+
+func main() {
+	// 加载配置文件
+	var err error
+	conf, err = config.LoadConfig("./conf/microservice.yaml")
+	if err != nil {
+		log.Fatalf("load config file faild: %s", err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	go run(ctx)
+	fmt.Println("awaiting system signal")
+	awaitingSystemSignal()
+	cancel()
+	stop()
+	fmt.Println("exiting")
+}
+
+// 监听系统终止信号
+func awaitingSystemSignal() {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGKILL, syscall.SIGTERM)
+
+	sig := <-sigChan
+	fmt.Println("close instance by:", sig)
+}
+
+func stop() {
+	err := servicecenter.Unregister(context.Background(), conf.Service)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func run(ctx context.Context) {
+	// 启动http监听
+	if conf.Service.Instance != nil {
+		go httpListener(conf.Service.Instance.ListenAddress)
+	}
+
+	// 初始化 service-center
+	servicecenter.InitRegistry(conf.Tenant.Domain+"/"+conf.Tenant.Project, conf.Registry)
+	serviceID, instanceID, err := servicecenter.Register(ctx, conf.Service)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("ServiceID:", serviceID)
+	log.Println("InstanceID:", instanceID)
+
+	conf.Service.ID = serviceID
+	if conf.Service.Instance != nil {
+		conf.Service.Instance.ID = instanceID
+	}
+
+	// 启动心跳
+	go servicecenter.Heartbeat(ctx, conf.Service)
+}
+
+func httpListener(listenAddress string) {
+	// 启动 http 监听
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("request hello")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("hello world"))
+	})
+	err := http.ListenAndServe(listenAddress, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
